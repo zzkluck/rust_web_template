d30d8db86f83c58f731ba0539bf8a0300f24b3b6
zzkluck <zzkluck@qq.com>
2024-10-25 10:05:51 +0800

    ✨:启用配置文件读取

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use zz_rust_web_template::configuration::get_configuration;
 use zz_rust_web_template::startup::run;
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let configuration = get_configuration().expect("Failed to read config.");
+    let listener =
+        std::net::TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
+    run(listener)?.await
 }
--- tests/test_health_check.rs
+++ tests/test_health_check.rs
@@ -3,12 +3,12 @@ use tokio;
 
 #[tokio::test]
 async fn health_check_return_200_and_no_content() {
-    spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8080/health_check")
+        .get(format!("{}/health_check", address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -17,8 +17,11 @@ async fn health_check_return_200_and_no_content() {
     assert_eq!(Some(0), response.content_length())
 }
 
-fn spawn_app() {
-    let server = zz_rust_web_template::startup::run().expect("Failed to bind address.");
-
+fn spawn_app() -> String {
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+    let server = zz_rust_web_template::startup::run(listener).expect("Failed to bind address.");
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }

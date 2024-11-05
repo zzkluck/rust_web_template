e62607b3f87aea598895d6826ff75090715e53ec
zzkluck <zzkluck@qq.com>
2024-11-05 15:02:40 +0800

    ✨: 添加了数据库相关配置项 - 从git中移除configuration.yaml - 添加了configuration.example.yaml - 在Configuration中添加数据库相关内容

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,26 @@
-#[derive(serde::Deserialize, Clone)]
+#[derive(serde::Deserialize)]
 pub struct Settings {
     pub application_port: u16,
     pub comfy_address: String,
+    pub database: DatabaseSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "mysql://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
